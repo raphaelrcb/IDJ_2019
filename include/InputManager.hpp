ed0e3d254d239c72c54cdfe712963e41d62bbce9
Unknown <raphaelrcbarbosa@gmail.com>
2019-04-12 15:07:50 -0300

    InputManager.hpp

---
+++ include/InputManager.hpp
@@ -0,0 +1,41 @@
+class InputManager{
+
+public:
+  InputManager();
+  ~InputManager();
+
+  void Update();
+
+  bool KeyPress(int key);
+  bool KeyRelease(int key);
+  bool IsKeyDown(int key);
+
+  bool MousePress(int key);
+  bool MouseRelease(int key);
+  bool IsMouseDown(int key);
+
+  int GetMouseX();
+  int GetMouseY();
+
+  bool QuitRequested();
+
+  static InputManager& GetInstance();
+
+private:
+  bool mouseState[6];
+  int mouseUpdate[6];
+  // bool keyState[416];
+  // int keyUpdate[416];
+
+  std::unordered_map<int, bool> keyState;
+  std::unordered_map<int, int> keyUpdate;
+
+  bool quitRequested;
+
+  int updateCounter;
+
+  int mouseX, mouseY;
+
+protected:
+
+};
