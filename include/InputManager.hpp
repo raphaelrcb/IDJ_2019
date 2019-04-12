b8d3e2a7da5da2eed8ce46d073b9a5792d986db2
Unknown <raphaelrcbarbosa@gmail.com>
2019-04-12 15:42:49 -0300

    Iniciou a implementação de InputManager.cpp

--- include/InputManager.hpp
+++ include/InputManager.hpp
@@ -1,3 +1,5 @@
+#include <unordered_map>
+
 class InputManager{
 
 public:
---
+++ src/InputManager.cpp
@@ -0,0 +1,68 @@
+#include "../include/InputManager.hpp"
+
+
+// InputManager *InputManager::instance = nullptr;
+InputManager::InputManager(){//inicializando todas a as variáveis
+
+  for (int i = 0; i < 6; i++) {
+    mouseUpdate[i] = 0;
+  }
+  quitRequested = false;
+  updateCounter = 0;
+  mouseX = 0
+  mouseY = 0;;
+
+}
+
+InputManager::~InputManager(){
+}
+
+
+void InputManager::Update(){
+}
+
+
+bool InputManager::KeyPress(int key){
+  return false;
+}
+
+bool InputManager::KeyRelease(int key){
+  return false;
+}
+
+bool InputManager::IsKeyDown(int key){
+  return false;
+}
+
+
+bool InputManager::MousePress(int key){
+  return false;
+}
+
+bool InputManager::MouseRelease(int key){
+  return false;
+}
+
+bool InputManager::IsMouseDown(int key){
+  return false;
+}
+
+
+int InputManager::GetMouseX(){
+  return 0;
+}
+
+int InputManager::GetMouseY(){
+  return 0;
+}
+
+
+bool InputManager::QuitRequested(){
+  return false;
+}
+
+
+InputManager& InputManager::GetInstance(){
+  static InputManager input;
+  return input;
+}
